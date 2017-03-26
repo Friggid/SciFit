e038a3557fec6e2ddba0ee5c51068809046b94e0
Tautvydas Milciunas <tmilciunas@gmail.com>
2017-03-26 16:08:19 +0300

    Stuff

--- SciFit/Controllers/AuthenticationController.cs
+++ SciFit/Controllers/AuthenticationController.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using SciFit.Models;
 
 namespace SciFit.Controllers
 {
@@ -19,5 +16,6 @@ namespace SciFit.Controllers
         {
             return View();
         }
+
     }
 }
---
+++ SciFit/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SciFit.Models
+{
+    public partial class User
+    {
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = "Required.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Required.")]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        public string Password { get; set; }
+        
+        [Required(ErrorMessage = "Required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string Email { get; set; }
+
+        public System.DateTime CreatedDate { get; set; }
+
+        public DateTime? LastLoginDate { get; set; }
+    }
+}
