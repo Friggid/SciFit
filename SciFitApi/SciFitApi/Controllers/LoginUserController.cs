38d1e04e24f6f03c659515fe7e6d525bd647302c
LukasIgn <lukas.ignatavicius2@gmail.com>
2017-04-02 22:37:00 +0300

    LoginUser controller

---
+++ SciFitApi/SciFitApi/Controllers/LoginUserController.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Description;
+using SciFitApi.Models;
+
+namespace SciFitApi.Controllers
+{
+    public class LoginUserController : ApiController
+    {
+        private readonly SciFitApiContext _db = new SciFitApiContext();
+
+        //POST api/User
+        [ResponseType(typeof(UsersModel))]
+        public IHttpActionResult PostUser(string username, string password)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var userId = (from x in _db.Users
+                          where x.UserName == username && x.Password == password
+                          select x.Id).FirstOrDefault();
+
+            var user = _db.Users.Find(userId);
+            if (user != null)
+            {
+                return Ok(user);
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+    }
+}
