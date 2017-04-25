bd452e980f01c1d67ca47734b5d4a09728f1aa40
Tautvydas Milciunas <tmilciunas@gmail.com>
2017-04-25 23:19:29 +0300

    Added validation
    Added validation

---
+++ SciFit/Models/UserModelPartialView.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SciFit.Models
+{
+    public class UserModelPartialView
+    {
+        public string Age { get; set; }
+
+        public string Weight { get; set; }
+
+        public string Height { get; set; }
+
+        [Required(ErrorMessage = "Please select difficulty!")]
+        public string Difficulty { get; set; }
+
+        public UserModel User { get; set; }
+    }
+}
