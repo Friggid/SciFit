371ed22f39b14c0ac5f48be2e2bd13530e7b6e26
Tautvydas <tmilciunas@gmail.com>
2017-04-26 02:38:18 +0300

    Jerky panel
    * User dublikavimo pataisymas
    
    * pass hash ir sporto plano kurimas/redagavimas API
    
    * Jerky admin/user panel
    
    Jerky admin/user panel

---
+++ SciFit/Controllers/AdministrationController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+using SciFit.Models;
+
+namespace SciFit.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        public ActionResult MainPanel(int roleId)
+        {
+            ViewBag.RoleId = roleId;
+            return View();
+        }
+
+        public ActionResult NavigateBackToProfile()
+        {
+            return RedirectToAction("SportPlan", "Sport");
+        }
+
+        public ActionResult AdminPartial()
+        {
+            return View();
+        }
+
+        public ActionResult AdmininstratePlan()
+        {
+            return PartialView("AdminPlansPartial");
+        }
+    }
+}
--- SciFit/Models/UserModelMainView.cs
+++ SciFit/Models/UserModelMainView.cs
@@ -12,10 +12,19 @@ namespace SciFit.Models
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Username is required!")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Must be between 3 and 20 characters!")]
         public string UserName { get; set; }
 
         [Required(ErrorMessage = "Password is required!")]
+        [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters!", MinimumLength = 5)]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is required!")]
+        [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
+        [DataType(DataType.Password, ErrorMessage = "Passwords must match")]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
 
         public UserModel User { get; set; }
     }
