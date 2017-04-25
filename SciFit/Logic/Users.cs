24d750389d6ea03d110592a87d4194d58409a178
LukasIgn <lukas.ignatavicius2@gmail.com>
2017-04-26 01:08:58 +0300

    pass hash ir sporto plano kurimas/redagavimas API

--- SciFit/Logic/Users.cs
+++ SciFit/Logic/Users.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using SciFit.Models;
 using System.Net.Http;
+using System.Security.Cryptography;
 using System.Text;
 using Newtonsoft.Json;
 
@@ -66,9 +67,10 @@ namespace SciFit.Logic
 
         public UserModel UserLogin(string userName, string password)
         {
+            var hashedPass = HashPass(password);
             using (var httpClient = new HttpClient())
             {
-                var response = httpClient.PostAsync("http://localhost:64483/api/LoginUser?username=" + userName + "&password=" + password, null).Result;
+                var response = httpClient.PostAsync("http://localhost:64483/api/LoginUser?username=" + userName + "&password=" + hashedPass, null).Result;
 
                 if (response.IsSuccessStatusCode)
                 {
@@ -82,5 +84,32 @@ namespace SciFit.Logic
                 return null;
             }
         }
+
+        #region Private helpers
+        private string HashPass(string pass)
+        {
+            var result = pass;
+            using (var md5Hash = MD5.Create())
+            {
+                // Convert the input string to a byte array and compute the hash.
+                var data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(pass));
+
+                // Create a new Stringbuilder to collect the bytes
+                // and create a string.
+                var sBuilder = new StringBuilder();
+
+                // Loop through each byte of the hashed data 
+                // and format each one as a hexadecimal string.
+                for (int i = 0; i < data.Length; i++)
+                {
+                    sBuilder.Append(data[i].ToString("x2"));
+                }
+
+                // Return the hexadecimal string.
+                result = sBuilder.ToString();
+            }
+            return result;
+        }
+        #endregion
     }
 }
---
+++ SciFitApi/SciFitApi/Controllers/SportTemplateController.cs
@@ -0,0 +1,81 @@
+﻿using System.Linq;
+using System.Data.Entity;
+using System.Web.Http;
+using System.Web.Http.Description;
+using SciFitApi.Models;
+
+namespace SciFitApi.Controllers
+{
+    public class SportTemplateController : ApiController
+    {
+        private readonly SciFitApiContext _db = new SciFitApiContext();
+
+        //GET api/SportTemplate
+        public IQueryable<SportTemplateModel> GetSport()
+        {
+            return _db.SportTemplate;
+        }
+
+        //GET api/SportTemplate/5
+        [ResponseType(typeof(SportTemplateModel))]
+        public IHttpActionResult GetSportTemplate(int id)
+        {
+            var sportTemplate = (from x in _db.SportTemplate
+                             where x.Id == id
+                             select x).SingleOrDefault();
+
+            if (sportTemplate == null)
+            {
+                return NotFound();
+            }
+            return Ok(sportTemplate);
+        }
+
+        //PUT api/SportTemplate/5
+        public IHttpActionResult PutSportTemplate(SportTemplateModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            _db.Entry(model).State = EntityState.Modified;
+            _db.SaveChanges();
+
+            return Ok(model);
+        }
+
+        //POST api/SportTemplate
+        [ResponseType(typeof(SportTemplateModel))]
+        public IHttpActionResult PostSportTemplate(SportTemplateModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            
+            _db.Entry(model).State = EntityState.Added;
+
+            _db.SaveChanges();
+
+            return Ok(model);
+        }
+
+        public IHttpActionResult DeleteSport(int id)
+        {
+            var sportTemplate = (from x in _db.SportTemplate
+                             where x.Id == id
+                             select x).SingleOrDefault();
+
+            if (sportTemplate == null)
+            {
+                return NotFound();
+            }
+
+            _db.Entry(sportTemplate).State = EntityState.Deleted;
+
+            _db.SaveChanges();
+            return Ok();
+        }
+    }
+}
