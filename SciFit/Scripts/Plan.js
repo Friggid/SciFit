bf4b8a2134f57106c59f9b4d44d7c0ecb6e46820
Tautvydas Milciunas <tmilciunas@gmail.com>
2017-04-29 22:12:17 +0300

    Fixed styling after login

--- SciFit/Scripts/Plan.js
+++ SciFit/Scripts/Plan.js
@@ -1,5 +1,6 @@
 ﻿$(document).ready(function () {
     $(".btn-pref .btn").click(function () {
+        console.log('button clickerd');
         $(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
         // $(".tab").addClass("active"); // instead of this do the below 
         $(this).removeClass("btn-default").addClass("btn-primary");
