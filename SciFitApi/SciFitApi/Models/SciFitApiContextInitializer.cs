e67eefc8f22616fd98525cfb582dc593b530fc02
Tautvydas Milciunas <tmilciunas@gmail.com>
2017-05-10 01:59:41 +0300

    Added new programs and fixed plan buttons alingment

--- SciFitApi/SciFitApi/Models/SciFitApiContextInitializer.cs
+++ SciFitApi/SciFitApi/Models/SciFitApiContextInitializer.cs
@@ -48,6 +48,17 @@ namespace SciFitApi.Models
                     },
                     new SportTemplateModel
                     {
+                        Name = "Prigulimai",
+                        Reps = "2X15",
+                        Image = "http://www.learningthealexandertechnique.com/wp-content/uploads/2012/09/drp_6072.jpg",
+                        Instructions = "Atsigulkite ir gulėkite.",
+                        Level = 1,
+                        Done = false,
+                        StartDate = DateTime.Now,
+                        Video = "www.youtube.com/embed/uPwHCGInwn4"
+                    },
+                    new SportTemplateModel
+                    {
                         Name = "Mirties kilpa",
                         Reps = "4X18",
                         Image = "https://www.bodybuilding.com/exercises/exerciseImages/sequences/2001/Male/m/2001_1.jpg",
@@ -87,6 +98,72 @@ namespace SciFitApi.Models
                         Image = "http://pasmama.tv3.lt/uploads/editor/image/1%281%29.jpg",
                         Instructions = "Atsistokite tiesia nugara, ištieskite rankas ir pritūpkite.",
                         Level = 3,
+                        Done = false,
+                        StartDate = DateTime.Now,
+                        Video = "www.youtube.com/embed/UXJrBgI2RxA"
+                    },
+                    new SportTemplateModel
+                    {
+                        Name = "DeadLift",
+                        Reps = "2X800",
+                        Image = "https://www.bodybuilding.com/exercises/exerciseImages/sequences/2001/Male/m/2001_1.jpg",
+                        Instructions = "Atsigulkite ant nugaros, sulenkite kelius, padėkite rankas už galvos ir prisitraukite prie sulenktų kelių.",
+                        Level = 4,
+                        Done = false,
+                        StartDate = DateTime.Now,
+                        Video = "www.youtube.com/embed/UXJrBgI2RxA"
+                    },
+                    new SportTemplateModel
+                    {
+                        Name = "Flying Bus",
+                        Reps = "2X120",
+                        Image = "http://pasmama.tv3.lt/uploads/editor/image/1%281%29.jpg",
+                        Instructions = "Atsistokite tiesia nugara, ištieskite rankas ir pritūpkite.",
+                        Level = 4,
+                        Done = false,
+                        StartDate = DateTime.Now,
+                        Video = "www.youtube.com/embed/UXJrBgI2RxA"
+                    },
+                    new SportTemplateModel
+                    {
+                        Name = "Sokantis briedis",
+                        Reps = "2X20",
+                        Image = "http://pasmama.tv3.lt/uploads/editor/image/1%281%29.jpg",
+                        Instructions = "Atsistokite ant triju koju ir padainuokite daina.",
+                        Level = 4,
+                        Done = false,
+                        StartDate = DateTime.Now,
+                        Video = "www.youtube.com/embed/UXJrBgI2RxA"
+                    },
+                    new SportTemplateModel
+                    {
+                        Name = "DeadLift",
+                        Reps = "2X800",
+                        Image = "https://www.bodybuilding.com/exercises/exerciseImages/sequences/2001/Male/m/2001_1.jpg",
+                        Instructions = "Atsigulkite ant nugaros, sulenkite kelius, padėkite rankas už galvos ir prisitraukite prie sulenktų kelių.",
+                        Level = 5,
+                        Done = false,
+                        StartDate = DateTime.Now,
+                        Video = "www.youtube.com/embed/UXJrBgI2RxA"
+                    },
+                    new SportTemplateModel
+                    {
+                        Name = "Flying Bus",
+                        Reps = "2X120",
+                        Image = "http://pasmama.tv3.lt/uploads/editor/image/1%281%29.jpg",
+                        Instructions = "Atsistokite tiesia nugara, ištieskite rankas ir pritūpkite.",
+                        Level = 5,
+                        Done = false,
+                        StartDate = DateTime.Now,
+                        Video = "www.youtube.com/embed/UXJrBgI2RxA"
+                    },
+                    new SportTemplateModel
+                    {
+                        Name = "Krintantis erelis",
+                        Reps = "2X16",
+                        Image = "http://pasmama.tv3.lt/uploads/editor/image/1%281%29.jpg",
+                        Instructions = "Kriskite!",
+                        Level = 5,
                         Done = false,
                         StartDate = DateTime.Now,
                         Video = "www.youtube.com/embed/UXJrBgI2RxA"
